de4524c8e605d7e49f09aa51a3e2ea8d79dee589
Zane Wesley <57075890+ZaneWesley@users.noreply.github.com>
2022-07-09 20:01:40 -0500

    2048 store update

---
+++ 2048/js/store_manager.js
@@ -0,0 +1,50 @@
+window.tempCoinStorage = {
+  _data: {},
+
+  setItem: function (id, val) {
+    return this._data[id] = String(val);
+  },
+
+  getItem: function (id) {
+    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+  },
+
+  removeItem: function (id) {
+    return delete this._data[id];
+  },
+
+  clear: function () {
+    return this._data = {};
+  }
+};
+
+function StoreManager() {
+	this.coinValueKey = "2048Coins";
+	this.themesKey = "2048Themes";
+
+	var LocalStorageManagerContructor = new LocalStorageManager();
+	var supported = LocalStorageManagerContructor.localStorageSupported();
+  	this.storage = supported ? window.localStorage : window.tempCoinStorage;
+}
+
+//Coin value setters/getters
+StoreManager.prototype.getCoins = function() {
+	return this.storage.getItem(this.coinValueKey) || 0;
+}
+
+StoreManager.prototype.setCoins = function (val) {
+  this.storage.setItem(this.coinValueKey, val);
+};
+
+//Theme setters/getters
+StoreManager.prototype.getThemes = function() {
+	return this.storage.getItem(this.themesKey) || 0;
+}
+
+StoreManager.prototype.setThemes = function (val) {
+  this.storage.setItem(this.themesKey, val);
+};
+
+//Set score on page load
+var Mngr = new StoreManager();
+document.querySelector('.coin-container').textContent = Mngr.getCoins();
